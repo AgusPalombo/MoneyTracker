1a5edbd329d679e8b8b008b1727172d3d2989b8c
Agustín Palombo <agpalombo99@gmail.com>
2024-11-06 10:54:41 -0300

    V0

--- money-tracker/src/App.js
+++ money-tracker/src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 
 function App() {
   return (
-   <div></div>
+    <main>
+        <h1>$400 <span>.00</span></h1>
+        <form>
+          <input type="text" placeholder={'+200000 nueva tv Samsung'}/>
+          <input type="datetime-local"/>
+          <input type="text" placeholder={'descripcion'}/>
+        </form>
+    </main>
   );
 }
 
